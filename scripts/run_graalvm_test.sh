b8804a1e2860a4ee4ad663768d880d85ce00cae7
Mridula Peddada <mpeddada@google.com>
2022-11-16 17:03:37 -0500

    add downstream check for orgpolicy

--- scripts/run_graalvm_test.sh
+++ scripts/run_graalvm_test.sh
@@ -1,32 +1,67 @@
 #!/bin/bash
 
+set -e
+
 CLIENT_LIBRARY=$1
 
 ## Get the directory of the build script
 scriptDir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 ## cd to the parent directory, i.e. the root of the git repo
 cd "${scriptDir}"/..
-pwd
-# Publish gax to local maven to make it available for downstream libraries
-#cd gax-java
-#./gradlew publishToMavenLocal
-#
-#cd "${scriptDir}"/..
-#cd java-shared-dependencies
-#mvn clean install -DskipTests
-#
-## Namespace (xmlns) prevents xmllint from specifying tag names in XPath
-#SHARED_DEPS_VERSION=$( sed -e 's/xmlns=".*"//' pom.xml | xmllint --xpath '/project/version/text()' - )
-#
-#if [ -z "${SHARED_DEPS_VERSION}" ]; then
-#  echo "Version is not found in pom.xml"
-#  exit 1
-#fi
-#
-#cd "${scriptDir}"/"${CLIENT_LIBRARY}"
 
-#mvn clean install -DskipTests
-#mvn test -Pnative
+# GAX: Publish gax to local maven to make it available for downstream libraries
+cd gax-java
+#git checkout update-gax
+# Read current gax version
+GAX_VERSION=$( ./gradlew -q :gax:properties | grep '^version: ' | cut -d' ' -f2 )
+./gradlew publishToMavenLocal
+
+# java-shared-dependencies
+cd "${scriptDir}"/..
+#git checkout update-gax
+pushd java-shared-dependencies/first-party-dependencies
+
+# replace version
+xmllint --shell pom.xml << EOF
+setns x=http://maven.apache.org/POM/4.0.0
+cd .//x:artifactId[text()="gax-bom"]
+cd ../x:version
+set ${GAX_VERSION}
+cd ../..
+cd .//x:artifactId[text()="gax-grpc"]
+cd ../x:version
+set ${GAX_VERSION}
+save pom.xml
+EOF
+
+cd ..
+mvn clean install -DskipTests
+
+# Namespace (xmlns) prevents xmllint from specifying tag names in XPath
+SHARED_DEPS_VERSION=$( sed -e 's/xmlns=".*"//' pom.xml | xmllint --xpath '/project/version/text()' - )
+
+if [ -z "${SHARED_DEPS_VERSION}" ]; then
+  echo "Version is not found in pom.xml"
+  exit 1
+fi
+
+# Library
+# Need to replace based on lib used
+git clone "https://github.com/googleapis/java-${CLIENT_LIBRARY}.git" --depth=1
+pushd java-"${CLIENT_LIBRARY}"
+
+# Replace shared-dependencies version
+xmllint --shell pom.xml << EOF
+setns x=http://maven.apache.org/POM/4.0.0
+cd .//x:artifactId[text()="google-cloud-shared-dependencies"]
+cd ../x:version
+set ${SHARED_DEPS_VERSION}
+save pom.xml
+EOF
+
+# Run native image tests
+mvn clean install -DskipTests
+mvn test -Pnative
 
 
 
